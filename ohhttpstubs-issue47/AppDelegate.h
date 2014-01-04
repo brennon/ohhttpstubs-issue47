ff44976520c1269d7dbeb2848a0b4e8f725b4d6c
Brennon Bortz <brennon@brennonbortz.com>
2014-01-04 17:10:47 -0500

    Initial Commit

---
+++ ohhttpstubs-issue47/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ohhttpstubs-issue47
+//
+//  Created by Brennon Bortz on 1/4/14.
+//  Copyright (c) 2014 Brennon Bortz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
